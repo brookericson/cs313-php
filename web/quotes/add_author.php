d8dfc0e6a1483a88f32acbbc450bc3ba70338fa0
brookericson <eri11006@byui.edu>
2017-10-25 23:41:01 -0600

    bootstrap fixed

--- web/quotes/add_author.php
+++ web/quotes/add_author.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php include $_SERVER['DOCUMENT_ROOT'].'/-cs313/web/quotes/modules/head.php'; ?>
+		<meta charset="utf-8">
+		<title>Quotes</title>
+		<link rel="stylesheet" href="css/style.css">
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<header>
-			<?php include $_SERVER['DOCUMENT_ROOT'].'/-cs313/web/quotes/modules/header.php'; ?>
+			<nav class="navbar navbar-default">
+				<div class="continer-fluid">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php?action=home">Home</a></li>				
+					<li><a href="index.php?action=quotes">Quotes</a></li>
+					<?php if(isset($_SESSION['loggedin'])) {
+						echo '<li><a href="index.php?action=account">My Account</a></li>';
+						echo '<li><a href="index.php?action=logout">Log Out</a></li>';
+						}
+						else {
+							echo '<li><a href="index.php?action=login">Login</a></li>';
+						}
+					?>
+				</ul>
+				</div>
+			</nav>
 		</header>
 		<main>
 			<h1>Create a New Author</h1>
