6999b146bd9ead47d4ade0c7ce855bdfc0062809
brookericson <eri11006@byui.edu>
2017-10-25 23:19:25 -0600

    fixed bootstrap

--- web/quotes/modules/head.php
+++ web/quotes/modules/head.php
@@ -1,8 +1,8 @@
 <meta charset="utf-8">
 <title>Quotes</title>
-<link rel="stylesheet" href="../css/style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
