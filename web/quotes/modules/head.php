1f8d3179b6d9faaa2606d627c4dff92924b9e516
brookericson <eri11006@byui.edu>
2017-10-25 23:12:59 -0600

    fixed bootstrap

--- web/quotes/modules/head.php
+++ web/quotes/modules/head.php
@@ -1,7 +1,6 @@
 <meta charset="utf-8">
 <title>Quotes</title>
-<link rel="stylesheet" href="css/style.css">
-<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
