6a80f55d001303bce439b26fbadf4130290a4305
brookericson <eri11006@byui.edu>
2017-11-11 21:19:42 -0700

    updated index.js file

--- web/myapp/index.js
+++ web/myapp/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var url = require('url');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,6 +17,8 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+console.log(url);
 
 app.get('/postage', function(request, response) {
 	handlePostage(request, response);
