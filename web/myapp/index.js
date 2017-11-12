eeda5eb3d7e1580423023a14f094fb7fa76e781c
brookericson <eri11006@byui.edu>
2017-11-11 19:49:36 -0700

    updated link

--- web/myapp/index.js
+++ web/myapp/index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('myapp/', function(request, response) {
+app.get('myapp/index.js', function(request, response) {
   response.render('form');
 });
 
